f45e048f952416bb6ffb3eb57b41f8425a5458c4
Nongoogle <essamsleem9@gmail.com>
2026-08-29 22:40:56 +0300

    Question02

--- Program.cs
+++ Program.cs
@@ -12,12 +12,24 @@
             #region Question1
             //1-Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it. 
 
-            //Book book = new Book();
-            //object obj = book;
+            //Book book1 = new Book();
+            //object obj = book1;
 
             //Console.WriteLine(obj);
             #endregion
 
+            #region Question2
+            //2-Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book. 
+
+            //Console.WriteLine(book1.ToString());
+
+            //Console.WriteLine(book1.Equals(book1));
+
+            //Console.WriteLine(book1.GetHashCode());
+
+            //Console.WriteLine(book1.GetType());
+
+            #endregion
 
         }
     }
